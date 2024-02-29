f2e66e7f1b25549cfc17595acbdaf255363468d7
Harsh0592 <142566475+Harsh0592@users.noreply.github.com>
2024-02-29 13:45:28 +0530

    Delete 09_Aug_Mern-main directory

--- 09_Aug_Mern-main/C++/Hello_C++.cpp
+++
@@ -1,7 +0,0 @@
-//First C++ Program
-#include<iostream> //"iostream" is a class
-using namespace std; // to use objects and variable like cout & cin
-
-main(){
-	cout<<"Hello World!!"; // for print the statments & value on output screen
-}
