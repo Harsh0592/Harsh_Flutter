422db3fd372a9b6aae55fbf72cdf12939948862d
Harsh0592 <harshdalasaniya@gmail.com>
2024-03-04 09:54:34 +0530

    Done

---
+++ Travel Guide Api/travel_api/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:travel_api/Screens/List.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
